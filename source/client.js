7fbfa762be6569f8f40b6c65e11b44ac3f44b9fb
Antonis Berkakis <aberkakis@gmail.com>
2017-08-01 23:06:28 +0100

    Using the first React App component

---
+++ source/client.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+import data from "./data.json";
+
+
+ReactDOM.render(<App />, document.getElementById('react'))
