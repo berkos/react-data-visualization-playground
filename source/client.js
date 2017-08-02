92c9de16aeb65cf13dc445d0049858ebd7e2fda5
Antonis Berkakis <aberkakis@gmail.com>
2017-08-02 22:13:58 +0100

    Create a map

--- source/client.js
+++ source/client.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import Map from './components/map';
 
-import data from "./data.json";
-
+// import data from "./data.json";
 //name={data.name} age={data.age}
+// ReactDOM.render(<App {...data}/>, document.getElementById('react'));
 
-
-ReactDOM.render(<App {...data}/>, document.getElementById('react'));
+ReactDOM.render(<Map />, document.getElementById('react'))
---
+++ source/components/map.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Map extends React.Component {
+  render() {
+    return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/K%C3%B6ppen-vereinfacht.svg/1440px-K%C3%B6ppen-vereinfacht.svg.png"/>
+  }
+}
