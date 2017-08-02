d9cb5bab22b107f291f5ffeb1384ce27778950cd
Antonis Berkakis <aberkakis@gmail.com>
2017-08-02 22:28:33 +0100

    Take click mouse out of the map

--- source/components/map.js
+++ source/components/map.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 export default class Map extends React.Component {
+  onClick(e) {
+    let img = e.target;
+    let point = {x: e.offsetX, y: e.offsetY};
+
+    if (!point.x) {
+      point = {
+        x: e.pageX - e.target.offsetLeft,
+        y: e.pageY - e.target.offsetTop
+      };
+    }
+
+    console.log(point);
+  }
+
   render() {
-    return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/K%C3%B6ppen-vereinfacht.svg/1440px-K%C3%B6ppen-vereinfacht.svg.png"/>
+    return <img onClick={this.onClick}
+      src="https://upload.wikimedia.org/wikipedia/commons/f/f4/Mercator_projection_SW.jpg"/>
   }
 }
