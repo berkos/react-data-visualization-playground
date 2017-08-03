4f9b43d5633d4443b8663cd89a1fe43efd5b5ad7
Antonis Berkakis <aberkakis@gmail.com>
2017-08-03 23:02:08 +0100

    Set up griddle

--- source/components/map.js
+++ source/components/map.js
@@ -1,6 +1,15 @@
 import React from "react";
+//import Griddle from 'griddle-react';
+var Griddle = require('griddle-react').default;
 
 export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+    this.state = {cities: []};
+  }
+
   onClick(e) {
     let img = e.target,
       point = {x: e.offsetX, y: e.offsetY},
@@ -19,8 +28,9 @@ export default class Map extends React.Component {
 
     lat = (point.y - (img.height / 2)) / -latSize;
     lon = (point.x - (img.width / 2)) / lonSize;
+    console.log(lat,lon);
 
-    let ws = `http://api.geonames.org/citiesJSON?north=${lat + r}&south=${lat - r}&east=${lon + r}&west=${lon - r}&lang=en&username=geek`;
+    let ws = `http://api.geonames.org/citiesJSON?formatted=true&north=${lat+r}&south=${lat-r}&east=${lon+r}&west=${lon-r}&lang=en&username=geek&&style=full`;
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', ws);
@@ -28,7 +38,8 @@ export default class Map extends React.Component {
     xhr.onload = () => {
       //this
       if (xhr.status === 200) {
-        console.log(xhr.responseText);
+        this.setState({cities: JSON.parse(xhr.responseText).geonames});
+
       } else {
         // ERROR
         console.log('Error', xhr);
@@ -39,7 +50,10 @@ export default class Map extends React.Component {
   }
 
   render() {
-    return <img onClick={this.onClick}
-      src="https://upload.wikimedia.org/wikipedia/commons/f/f4/Mercator_projection_SW.jpg"/>
+
+    return <div>
+      <img onClick={this.onClick} src="https://upload.wikimedia.org/wikipedia/commons/f/f4/Mercator_projection_SW.jpg"/>
+      <Griddle data={this.state.cities} />
+    </div>;
   }
 }
