0aca5fcd75643c684b9453789f972ae1a1483855
Antonis Berkakis <aberkakis@gmail.com>
2017-08-05 00:46:03 +0100

    Create dummy data

--- source/client_chart.js
+++ source/client_chart.js
@@ -2,4 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from './components/chart';
 
-ReactDOM.render(<Chart />, document.getElementById('react'));
+const data = {
+  type: "bar",
+  data: {
+    labels: ["Ben", "James", "Marry", "Sam", "Gloria"],
+    dataset: [
+      {
+        label: 'Fun',
+        data: [80, 40, 21, 44, 92],
+        backgroundColor: "rgba(255,0,0,.2)",
+        borderWidth: 1
+      },
+      {
+        label: 'Smart',
+        data: [87, 40, 21, 94, 32],
+        backgroundColor: "rgba(0,0,255,.2)",
+        borderWidth: 1
+      },
+      {
+        label: 'Fun',
+        data: [30, 50, 71, 14, 52],
+        backgroundColor: "rgba(0,255,0,.2)",
+        borderWidth: 3
+      },
+    ]
+  },
+  options: {
+
+  }
+};
+ReactDOM.render(<Chart {...data}/>, document.getElementById('react'));
