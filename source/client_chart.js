d77bfc8bebed24b71558df8c6b1735be8187eb70
Antonis Berkakis <aberkakis@gmail.com>
2017-08-07 00:47:12 +0100

    Extract logic in a function

--- source/client_chart.js
+++ source/client_chart.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from './components/chart';
-import data2_csv from '../data/data.csv';
-
-// Shift the first item
-let obj = data2_csv.shift(),
-  formatedData = {labels: [obj.name], datasets: []},
-  map = new Map();
-
-delete obj.name;
-for (let item in obj) {
-  let dataset = {label: item, data: [obj.item]};
-  formatedData.datasets.push(dataset);
-  map[item] = dataset;
-}
-
-data2_csv.forEach( (obj, i, a) => {
-  formatedData.labels.push(obj.name);
-  delete obj.name;
-  for (let item in obj) {
-    map[item].data.push(obj[item]);
-  }
-});
+import rawData from '../data/data.csv';
+import {a2c} from './utils/a2chart'
 
 const data2 = {
   type: "bar",
-  data: formatedData,
+  data: a2c(rawData),
   options: {}
 };
 
